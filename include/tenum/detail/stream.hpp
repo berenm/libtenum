e8a29054b34ace211de5530c8d7e69880e342c78
Beren Minor <beren.minor+git@gmail.com>
2011-05-03 17:11:59 +0200

    Updated license.

--- include/tenum/detail/stream.hpp
+++ include/tenum/detail/stream.hpp
@@ -1,6 +1,8 @@
 /**
  * @file
- * @date May 3, 2011
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
  */
 
 #ifndef TENUM_DETAIL_STREAM_HPP_
--- test/test_c0x.cpp
+++ test/test_c0x.cpp
@@ -1,6 +1,8 @@
 /**
  * @file
- * @date Apr 20, 2011
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
  */
 
 #define BOOST_TEST_MODULE TENUM_test
