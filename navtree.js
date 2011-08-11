854053d627a7740f75e0c547e69e48a590e3f762
Beren Minor <beren.minor+git@gmail.com>
2011-08-11 21:56:21 +0200

    Added doxygen generated doc.

---
+++ navtree.js
@@ -0,0 +1,334 @@
+var NAVTREE =
+[
+  [ "libtenum", "index.html", [
+    [ "Related Pages", "pages.html", [
+      [ "Todo List", "todo.html", null ]
+    ] ],
+    [ "Modules", "modules.html", [
+      [ "Bit fields test cases", "group__bit__field__tests.html", null ],
+      [ "Dynamic enums test cases", "group__dynamic__enum__tests.html", null ],
+      [ "Static enums test cases", "group__static__enum__tests.html", null ]
+    ] ],
+    [ "Class List", "annotated.html", [
+      [ "tenum::enum_helper< EnumType >", "structtenum_1_1enum__helper.html", null ],
+      [ "tenum::is_bit_field< EnumType >", "structtenum_1_1is__bit__field.html", null ],
+      [ "tenum::is_dynamic< EnumType >", "structtenum_1_1is__dynamic.html", null ],
+      [ "test_bit_field_deserialization", "classtest__bit__field__deserialization.html", null ],
+      [ "test_bit_field_empty_deserialization", "classtest__bit__field__empty__deserialization.html", null ],
+      [ "test_bit_field_empty_serialization", "classtest__bit__field__empty__serialization.html", null ],
+      [ "test_bit_field_from_int_serialization", "classtest__bit__field__from__int__serialization.html", null ],
+      [ "test_bit_field_from_or_serialization", "classtest__bit__field__from__or__serialization.html", null ],
+      [ "test_bit_field_integers_bitwise_and", "classtest__bit__field__integers__bitwise__and.html", null ],
+      [ "test_bit_field_integers_bitwise_and_assign", "classtest__bit__field__integers__bitwise__and__assign.html", null ],
+      [ "test_bit_field_integers_bitwise_or", "classtest__bit__field__integers__bitwise__or.html", null ],
+      [ "test_bit_field_integers_bitwise_or_assign", "classtest__bit__field__integers__bitwise__or__assign.html", null ],
+      [ "test_bit_field_integers_bitwise_xor", "classtest__bit__field__integers__bitwise__xor.html", null ],
+      [ "test_bit_field_integers_bitwise_xor_assign", "classtest__bit__field__integers__bitwise__xor__assign.html", null ],
+      [ "test_bit_field_integers_eq", "classtest__bit__field__integers__eq.html", null ],
+      [ "test_bit_field_integers_neq", "classtest__bit__field__integers__neq.html", null ],
+      [ "test_bit_field_or_variable", "classtest__bit__field__or__variable.html", null ],
+      [ "test_bit_field_serialization_explicit", "classtest__bit__field__serialization__explicit.html", null ],
+      [ "test_bit_field_xor_variable", "classtest__bit__field__xor__variable.html", null ],
+      [ "test_dynamic_enum_correct_deserialization", "classtest__dynamic__enum__correct__deserialization.html", null ],
+      [ "test_dynamic_enum_deserialization", "classtest__dynamic__enum__deserialization.html", null ],
+      [ "test_dynamic_enum_empty_deserialization", "classtest__dynamic__enum__empty__deserialization.html", null ],
+      [ "test_dynamic_enum_from_integer_serialization", "classtest__dynamic__enum__from__integer__serialization.html", null ],
+      [ "test_dynamic_enum_from_offset_serialization", "classtest__dynamic__enum__from__offset__serialization.html", null ],
+      [ "test_dynamic_enum_integers_bitwise_and", "classtest__dynamic__enum__integers__bitwise__and.html", null ],
+      [ "test_dynamic_enum_integers_bitwise_and_assign", "classtest__dynamic__enum__integers__bitwise__and__assign.html", null ],
+      [ "test_dynamic_enum_integers_bitwise_or", "classtest__dynamic__enum__integers__bitwise__or.html", null ],
+      [ "test_dynamic_enum_integers_bitwise_or_assign", "classtest__dynamic__enum__integers__bitwise__or__assign.html", null ],
+      [ "test_dynamic_enum_integers_bitwise_xor", "classtest__dynamic__enum__integers__bitwise__xor.html", null ],
+      [ "test_dynamic_enum_integers_bitwise_xor_assign", "classtest__dynamic__enum__integers__bitwise__xor__assign.html", null ],
+      [ "test_dynamic_enum_integers_eq", "classtest__dynamic__enum__integers__eq.html", null ],
+      [ "test_dynamic_enum_integers_neq", "classtest__dynamic__enum__integers__neq.html", null ],
+      [ "test_dynamic_enum_out_of_range_deserialization", "classtest__dynamic__enum__out__of__range__deserialization.html", null ],
+      [ "test_dynamic_enum_out_of_range_serialization", "classtest__dynamic__enum__out__of__range__serialization.html", null ],
+      [ "test_dynamic_enum_serialization_explicit", "classtest__dynamic__enum__serialization__explicit.html", null ],
+      [ "test_dynamic_enum_unknown_serialization", "classtest__dynamic__enum__unknown__serialization.html", null ],
+      [ "test_static_enum_deserialization", "classtest__static__enum__deserialization.html", null ],
+      [ "test_static_enum_empty_deserialization", "classtest__static__enum__empty__deserialization.html", null ],
+      [ "test_static_enum_from_enum_serialization", "classtest__static__enum__from__enum__serialization.html", null ],
+      [ "test_static_enum_from_int_explicit_serialization", "classtest__static__enum__from__int__explicit__serialization.html", null ],
+      [ "test_static_enum_integers_bitwise_and", "classtest__static__enum__integers__bitwise__and.html", null ],
+      [ "test_static_enum_integers_bitwise_and_assign", "classtest__static__enum__integers__bitwise__and__assign.html", null ],
+      [ "test_static_enum_integers_bitwise_or", "classtest__static__enum__integers__bitwise__or.html", null ],
+      [ "test_static_enum_integers_bitwise_or_assign", "classtest__static__enum__integers__bitwise__or__assign.html", null ],
+      [ "test_static_enum_integers_bitwise_xor", "classtest__static__enum__integers__bitwise__xor.html", null ],
+      [ "test_static_enum_integers_bitwise_xor_assign", "classtest__static__enum__integers__bitwise__xor__assign.html", null ],
+      [ "test_static_enum_integers_eq", "classtest__static__enum__integers__eq.html", null ],
+      [ "test_static_enum_integers_neq", "classtest__static__enum__integers__neq.html", null ],
+      [ "test_static_enum_unknown_deserialization", "classtest__static__enum__unknown__deserialization.html", null ],
+      [ "test_static_enum_unknown_integer_serialization", "classtest__static__enum__unknown__integer__serialization.html", null ],
+      [ "test_static_enum_unknown_serialization", "classtest__static__enum__unknown__serialization.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "tenum", "namespacetenum.html", null ]
+    ] ],
+    [ "File List", "files.html", [
+      [ "include/tenum.hpp", "tenum_8hpp.html", null ],
+      [ "include/tenum/bit_field.hpp", "bit__field_8hpp.html", null ],
+      [ "include/tenum/default.hpp", "default_8hpp.html", null ],
+      [ "include/tenum/dynamic_enum.hpp", "dynamic__enum_8hpp.html", null ],
+      [ "include/tenum/shortcut.hpp", "shortcut_8hpp.html", null ],
+      [ "include/tenum/static_enum.hpp", "static__enum_8hpp.html", null ],
+      [ "include/tenum/detail/enum.hpp", "enum_8hpp.html", null ],
+      [ "include/tenum/detail/helper.hpp", "helper_8hpp.html", null ],
+      [ "include/tenum/detail/operator.hpp", "operator_8hpp.html", null ],
+      [ "include/tenum/detail/stream.hpp", "stream_8hpp.html", null ],
+      [ "include/tenum/detail/type.hpp", "type_8hpp.html", null ],
+      [ "test/test_bit_field.cpp", "test__bit__field_8cpp.html", null ],
+      [ "test/test_dynamic_enum.cpp", "test__dynamic__enum_8cpp.html", null ],
+      [ "test/test_static_enum.cpp", "test__static__enum_8cpp.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
